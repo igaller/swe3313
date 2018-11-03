f06d859400a197d9dbe2dc5b326557ef0407ddc7
evanngevara <44121005+evanngevara@users.noreply.github.com>
2018-11-02 23:49:19 -0400

    Add files via upload

---
+++ src/CreditCard.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+
+    class CreditCard
+    {
+        private String creditCardID = null;
+        private String creditCardName = null;
+        private String creditCardNumber = null;
+        private String creditCardExpiration = null;
+
+        public CreditCard(String creditCardID)
+        {
+            this.creditCardID = creditCardID;
+        }
+
+        public string CreditCardID
+        {
+            get
+            {
+                return creditCardID;
+            }
+
+            set
+            {
+                creditCardID = value;
+            }
+        }
+
+        public string CreditCardName
+        {
+            get
+            {
+                return creditCardName;
+            }
+
+            set
+            {
+                creditCardName = value;
+            }
+        }
+
+        public string CreditCardNumber
+        {
+            get
+            {
+                return creditCardNumber;
+            }
+
+            set
+            {
+                creditCardNumber = value;
+            }
+        }
+
+        public string CreditCardExpiration
+        {
+            get
+            {
+                return creditCardExpiration;
+            }
+
+            set
+            {
+                creditCardExpiration = value;
+            }
+        }
+
+        public String toString()
+        {
+            String[] creditCardData = new string[4];
+            creditCardData[0] = Utility.quote(creditCardNumber);
+            creditCardData[1] = Utility.quote(CreditCardName);
+            creditCardData[2] = Utility.quote(CreditCardNumber);
+            creditCardData[1] = Utility.quote(CreditCardExpiration);
+
+
+            String collectCreditCard = string.Join(",", creditCardData);
+            return collectCreditCard;
+        }
+    }
+}
---
+++ src/Drink.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Pizza
+{
+    class Drink
+    {
+        private String drinkName = null;
+        private double drinkPrice = null;
+
+        public string DrinkName
+        {
+            get
+            {
+                return drinkName;
+            }
+
+            set
+            {
+                drinkName = value;
+            }
+        }
+
+        public double DrinkPrice
+        {
+            get
+            {
+                return drinkPrice;
+            }
+
+            set
+            {
+                drinkPrice = value;
+            }
+        }
+
+        public String toString()
+        {
+            String[] drinkData = new string[4];
+            drinkData[0] = Utility.quote(creditCardNumber);
+            drinkData[1] = Utility.quote(CreditCardName);
+            drinkData[2] = Utility.quote(CreditCardNumber);
+            drinkData[1] = Utility.quote(CreditCardExpiration);
+
+            String collectDrink = string.Join(",", drinkData);
+            return collectDrink;
+        }
+    }
+}
